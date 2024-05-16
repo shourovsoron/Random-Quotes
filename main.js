eb6f49b3d5f1147809f23dbacf65132f743c896f
Shourov Soron <shourovhossainsoron@gmail.com>
2024-05-16 22:43:10 +0600

    Second Commit

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const AuthorName = document.querySelector(".author");
 const tagDisplay = document.getElementById("tags");
 const TagBox = document.getElementById("tagBox");
 const Quotebtn = document.getElementById("quotebtn");
+const tweetbtn = document.getElementById("tweet");
 var tags = "";
+var Quote = "";
 const letters = new Set([]);
 console.log(BlockDisplay);
 
@@ -13,6 +15,7 @@ async function showQuote(url){
     var data = await response.json();
     console.log(data);
     BlockDisplay.innerText='"'+data[0].content+'"';
+    Quote=data[0].content
     AuthorName.innerText=data[0].author;
     // tagDisplay.innerText=data[0].tags;
     
@@ -47,15 +50,27 @@ TagBox.addEventListener("click", (e)=>{
     }
     
     showQuote(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
-    console.log(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
+    // console.log(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
 
-})
+});
+
+function tweet(){
+    const sharelink=`https://twitter.com/intent/tweet?text=${Quote}`;
+    window.open(sharelink)
+}
+// function facebook(){
+//     const sharelink=`https://www.facebook.com/intent/post?text=${Quote}`;
+//     window.open(sharelink)
+// }
+
+
+
 
 Quotebtn.addEventListener("click", ()=>{
 
     showQuote(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
-    console.log(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
+    // console.log(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
 })
 
 showQuote(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
-console.log(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
+// console.log(`https://api.quotable.io/quotes/random?maxLength=70&tags=${tags}`);
